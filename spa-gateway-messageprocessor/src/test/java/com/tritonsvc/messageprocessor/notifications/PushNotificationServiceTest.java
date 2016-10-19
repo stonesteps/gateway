92dd70c2f00fdd176682d8670e6bee6a0c43f023
marek <marek@digduck.com>
2016-10-19 18:05:22 +0200

    test refactoring, using CountDownLatch to wait for the pushPayload to be called (otherwise the test may fail)

--- spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/notifications/PushNotificationServiceTest.java
+++ spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/notifications/PushNotificationServiceTest.java
@@ -1,9 +1,14 @@
 package com.tritonsvc.messageprocessor.notifications;
 
 import com.bwg.iot.model.Alert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 /**
  * Created by holow on 18.10.2016.
@@ -11,10 +16,16 @@ import org.mockito.Mockito;
 public class PushNotificationServiceTest {
 
     @Test
-    @Ignore
     public void testPushNotificationService() throws Exception {
         final ApnsSenderBuilder builder = Mockito.mock(ApnsSenderBuilder.class);
         final ApnsSender sender = Mockito.mock(ApnsSender.class);
+
+        // with this, test will wait for pushPayload method to be called
+        final CountDownLatch cdl = new CountDownLatch(1);
+        doAnswer(invocation -> {
+            cdl.countDown();
+            return null;
+        }).when(sender).pushPayload(any(), any());
 
         Mockito.when(builder.build()).thenReturn(sender);
 
@@ -26,7 +37,9 @@ public class PushNotificationServiceTest {
         alert.setSeverityLevel("ERROR");
         service.pushApnsAlertNotification("token", alert);
 
+        // wait for pushPayload method to be called
+        cdl.await(5, TimeUnit.SECONDS);
+
         Mockito.verify(sender).pushPayload("token", "{\"aps\":{\"alert\":\"Water too hot\",\"sound\":\"default\",\"category\":\"SPA ALERT\"}}");
     }
-
 }
