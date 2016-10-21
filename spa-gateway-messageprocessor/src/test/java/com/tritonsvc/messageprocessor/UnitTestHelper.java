606ce6500692cf5371998c34be1ff7fea827a556
marek <marek@digduck.com>
2016-10-21 08:30:32 +0200

    saving alerts from fault logs using embedded mongo for unit testing spring boot versionupgrade

--- spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/UnitTestHelper.java
+++ spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/UnitTestHelper.java
@@ -4,8 +4,6 @@ import com.bwg.iot.model.Component;
 import com.bwg.iot.model.Component.ComponentType;
 import com.bwg.iot.model.Spa;
 import com.bwg.iot.model.SpaCommand;
-import com.github.fakemongo.Fongo;
-import com.mongodb.Mongo;
 import com.tritonsvc.messageprocessor.mongo.repository.ComponentRepository;
 import com.tritonsvc.messageprocessor.mongo.repository.SpaCommandRepository;
 import com.tritonsvc.messageprocessor.mongo.repository.SpaRepository;
@@ -13,7 +11,6 @@ import io.moquette.server.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -26,7 +23,7 @@ import java.util.UUID;
  * Helper class for unit testting.
  */
 @Configuration
-public class UnitTestHelper extends AbstractMongoConfiguration {
+public class UnitTestHelper {
 
     @Autowired
     private Environment env;
@@ -41,16 +38,6 @@ public class UnitTestHelper extends AbstractMongoConfiguration {
     private SpaCommandRepository spaCommandRepository;
 
     private final Server mqttServer = new Server();
-
-    @Override
-    protected String getDatabaseName() {
-        return env.getRequiredProperty("mongo.db.name");
-    }
-
-    @Override
-    public Mongo mongo() throws Exception {
-        return new Fongo(getDatabaseName()).getMongo();
-    }
 
     @PostConstruct
     public void startMqttBroker() throws Exception {
@@ -80,7 +67,7 @@ public class UnitTestHelper extends AbstractMongoConfiguration {
     public SpaCommand createSpaCommand(Spa spa, int requestType, HashMap<String, String> values) {
         final SpaCommand command = new SpaCommand();
         HashMap<String, String> meta = new HashMap<>(3);
-        meta.put("Requested By","Dan");
+        meta.put("Requested By", "Dan");
         meta.put("Via", "mobile");
         command.setSpaId(spa.get_id());
         command.setSentTimestamp(new Date());
