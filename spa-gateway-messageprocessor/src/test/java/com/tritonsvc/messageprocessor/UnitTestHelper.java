7b1d3d36422f2b6f3fa4940fa5d18b63a8262b06
http://dan.seibert@tritonsvc.com@iotdev05.bi.local <dan.seibert@tritonsvc.com>
2016-10-25 19:51:27 -0700

    backing out earlier merge that updates spring boot version and test mongo

--- spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/UnitTestHelper.java
+++ spa-gateway-messageprocessor/src/test/java/com/tritonsvc/messageprocessor/UnitTestHelper.java
@@ -4,6 +4,8 @@ import com.bwg.iot.model.Component;
 import com.bwg.iot.model.Component.ComponentType;
 import com.bwg.iot.model.Spa;
 import com.bwg.iot.model.SpaCommand;
+import com.github.fakemongo.Fongo;
+import com.mongodb.Mongo;
 import com.tritonsvc.messageprocessor.mongo.repository.ComponentRepository;
 import com.tritonsvc.messageprocessor.mongo.repository.SpaCommandRepository;
 import com.tritonsvc.messageprocessor.mongo.repository.SpaRepository;
@@ -11,6 +13,7 @@ import io.moquette.server.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -23,7 +26,7 @@ import java.util.UUID;
  * Helper class for unit testting.
  */
 @Configuration
-public class UnitTestHelper {
+public class UnitTestHelper extends AbstractMongoConfiguration {
 
     @Autowired
     private Environment env;
@@ -38,6 +41,16 @@ public class UnitTestHelper {
     private SpaCommandRepository spaCommandRepository;
 
     private final Server mqttServer = new Server();
+
+    @Override
+    protected String getDatabaseName() {
+        return env.getRequiredProperty("mongo.db.name");
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new Fongo(getDatabaseName()).getMongo();
+    }
 
     @PostConstruct
     public void startMqttBroker() throws Exception {
